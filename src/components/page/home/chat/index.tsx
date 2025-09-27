6934e23c3a7c22260adb1ca4d7435775cbe6e065
Liqy <3262978839@qq.com>
2025-09-27 20:05:56 +0800

    feat(聊天界面): 添加悬停显示时间和双击复制功能
    新增useHover和useDebounceCallback等自定义hook
    添加react-hot-toast用于操作反馈
    优化聊天界面样式和交互体验

--- src/components/page/home/chat/index.tsx
+++ src/components/page/home/chat/index.tsx
@@ -6,7 +6,7 @@ import {
   Smile,
   Paperclip,
   Mic,
-  MoreVertical
+  MoreVertical,
 } from "lucide-react";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { Button } from "@/components/ui/button";
@@ -153,11 +153,11 @@ const WeChatInterface = () => {
 
   return (
     <>
-      <div className="w-0 md:w-80 border-r border-primary-foreground/50 overflow-hidden transition-all duration-300">
+      <div className="w-0 border-0 md:w-80 md:border-r border-primary-foreground/50 overflow-hidden transition-all duration-300">
         {/* 中间联系人列表 */}
         <div className="flex flex-col">
           {/* 搜索栏 */}
-          <div className="p-4 border-b border-primary-foreground/50 h-16">
+          <div className="p-4 border-b border-r border-primary-foreground/50 h-16">
             <div className="relative">
               <Search className="absolute left-3 top-1/2 transform -translate-y-1/2 w-4 h-4 " />
               <Input placeholder="搜索联系人或聊天记录" className="pl-10" />
@@ -182,7 +182,7 @@ const WeChatInterface = () => {
       {/* 右侧聊天区域 */}
       <div className="flex-1 flex flex-col min-w-[300px] md:min-w-[400px]">
         {/* 聊天头部 */}
-        <div className="border-b border-primary-foreground/50  px-6 py-4 flex items-center justify-between h-16">
+        <div className="border-b border-primary-foreground/50 px-6 py-4 flex items-center justify-between h-16">
           <div className="flex items-center space-x-3">
             <Avatar className="w-10 h-10">
               <AvatarImage src={contacts[selectedChat]?.avatar} />
@@ -229,16 +229,6 @@ const WeChatInterface = () => {
                       <AvatarFallback>TA</AvatarFallback>
                     </Avatar>
                   )}
-
-                  {/* <div
-                    className={`rounded-lg px-4 py-2 ${
-                      msg.sender === "me"
-                        ? "0 "
-                        : "border border-primary-foreground/50"
-                    }`}
-                  >
-                    <p className="text-sm">{msg.content}</p>
-                  </div> */}
                   <Chatbox msg={msg} />
                 </div>
               </div>
