eed4f6017d45a576793a17ec713a64f87bdf0fa5
Liqy <3262978839@qq.com>
2025-09-28 20:45:12 +0800

    style(ui): 为聊天框和根布局添加文本选择样式
    为聊天消息内容添加可选中样式并设置选中背景色
    在根布局容器上禁用文本选择功能

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,18 +1,21 @@
-import * as React from 'react'
-import { Outlet, createRootRoute } from '@tanstack/react-router'
-import { TanStackRouterDevtools } from '@tanstack/react-router-devtools'
-import { Toaster } from 'react-hot-toast'
+import * as React from "react";
+import { Outlet, createRootRoute } from "@tanstack/react-router";
+import { TanStackRouterDevtools } from "@tanstack/react-router-devtools";
+import { Toaster } from "react-hot-toast";
 export const Route = createRootRoute({
   component: RootComponent,
-})
+});
 
 function RootComponent() {
   return (
     <React.Fragment>
       {/* <div>Hello "__root"!</div> */}
-      <Outlet />
-      <Toaster />
-      <TanStackRouterDevtools position="top-right" />
+
+      <div className="select-none">
+        <Outlet />
+        <Toaster />
+        <TanStackRouterDevtools position="top-right" />
+      </div>
     </React.Fragment>
-  )
+  );
 }
